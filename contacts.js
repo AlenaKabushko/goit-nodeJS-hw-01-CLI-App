0beb60959661ae850df17628dd79895fdeb6fc5c
Alena <100042777+AlenaKabushko@users.noreply.github.com>
2022-12-22 22:25:01 +0200

    add invoke action

--- contacts.js
+++ contacts.js
@@ -6,9 +6,7 @@ const contactsPath = path.normalize("./db/contacts.json");
 
 async function listContacts() {
     try {
-        console.log('list contacts:')
         const data = await fs.readFile(contactsPath, "utf8");
-        //console.log('JSON.parse(data)', JSON.parse(data));
         return JSON.parse(data)
     } catch (error) {
         console.log('error', error);
@@ -18,23 +16,18 @@ async function listContacts() {
 
 async function getContactById(contactId) {
     try {
-        console.log('contact Id:');
         const contacts = await listContacts();
-        console.log(contacts.find((obj) => obj.id === contactId.toString())) 
-        return contacts.find((obj) => obj.id === contactId.toString())
+        return contacts.find((obj) => obj.id === contactId)
     } catch (error) {
         console.log('error', error);
     }
 }
 
 async function removeContact(contactId) {
-    console.log('remove Contact:')
-    
     try {
         const contacts = await listContacts();
         const updateContacts =
             contacts.filter((obj) => obj.id !== contactId.toString());
-        console.log(updateContacts)
         await fs.writeFile(contactsPath, JSON.stringify(updateContacts, null, '\t'), "utf-8")
         return 
     } catch (error) {
@@ -43,12 +36,11 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-    console.log('add Contact:')
     try {
         const id = uniqid();
         const contacts = await listContacts();
         contacts.push({ id, name, email, phone })
-        console.log(contacts)
+        return contacts
     } catch (error) {
         console.log('error', error);
     }
